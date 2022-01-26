8b8eed06df407ce18b9085d3df45d3484dbd37cf
dilzor28 <dilzor28@gmail.com>
2022-01-26 18:46:20 -0500

    dotnet test

---
+++ CodeFirst/Migrations/202201260114524_InitialMigrations.cs
@@ -0,0 +1,72 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrations : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Level = c.Int(nullable: false),
+                        FullPrice = c.Single(nullable: false),
+                        Author_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.Author_Id)
+                .Index(t => t.Author_Id);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CourseTags",
+                c => new
+                    {
+                        Tag_Id = c.Int(nullable: false),
+                        Course_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Tag_Id, t.Course_Id })
+                .ForeignKey("dbo.Tags", t => t.Tag_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Courses", t => t.Course_Id, cascadeDelete: true)
+                .Index(t => t.Tag_Id)
+                .Index(t => t.Course_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CourseTags", "Course_Id", "dbo.Courses");
+            DropForeignKey("dbo.CourseTags", "Tag_Id", "dbo.Tags");
+            DropForeignKey("dbo.Courses", "Author_Id", "dbo.Authors");
+            DropIndex("dbo.CourseTags", new[] { "Course_Id" });
+            DropIndex("dbo.CourseTags", new[] { "Tag_Id" });
+            DropIndex("dbo.Courses", new[] { "Author_Id" });
+            DropTable("dbo.CourseTags");
+            DropTable("dbo.Tags");
+            DropTable("dbo.Courses");
+            DropTable("dbo.Authors");
+        }
+    }
+}
